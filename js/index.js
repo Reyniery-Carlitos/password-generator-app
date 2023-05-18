113e1758612893e9ab5605e74ec8adb89f2971f3
Carlos Rubio <calitod91@gmail.com>
2023-05-18 15:50:17 -0600

    Reescribiendo nombres de variables y limpiando el codigo

--- js/index.js
+++ js/index.js
@@ -1,18 +1,25 @@
+// DOM Elements
 const formValues = document.getElementById('container-inputs')
 const passGen = document.getElementById('passwordGen');    
 const numberRange = document.getElementById('numberRange');
 const lenghSelected = document.getElementById('lengthSelected');
 const passComplex = document.getElementById('passComplex')
-let lengthPass = 4;
 
+// Min password length 
+let passLength = 4;
+
+// Array of user's options selected 
 const optionsChecked = []
-const items = {
+
+// Password rules
+const rules = {
     "uppercase": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     "lowercase": "abcdefghijklmnopqrstuvwxyz",
     "symbols": "`~!@#$%^&*()-_=+|[]{}'.:,;<>/?,",
     "numbers": "0123456789"
 }
 
+// Password complexity
 const passComplexOptions = {
     1: "Weak",
     2: "Medium",
@@ -20,11 +27,16 @@ const passComplexOptions = {
     4: "Strong"
 }
 
+// Gets the length of the password selected by the user and updates the DOM element
 const getLength = (e) => {
-    lengthPass = numberRange.value;
-    lenghSelected.innerHTML = lengthPass
+    passLength = numberRange.value;
+    lenghSelected.innerHTML = passLength
 }
 
+// Returns a random number given the max value
+const getRandomNumber = max => Math.floor(Math.random() * max);
+
+// Manipulate form info
 formValues.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -39,16 +51,17 @@ formValues.addEventListener("submit", (e) => {
     }
 
     const optionChekedLength = optionsChecked.length
-    for (let i = 0, len = lengthPass; i < len; i++) {
-        const itemSelected = items[optionsChecked[Math.floor(Math.random() * optionChekedLength)]]
-        passGenerated += itemSelected.charAt(Math.floor(Math.random() * itemSelected.length))
+    for (let i = 0, len = passLength; i < len; i++) {
+        const ruleSelected = rules[optionsChecked[getRandomNumber(optionChekedLength)]]
+        passGenerated += ruleSelected.charAt(getRandomNumber(ruleSelected.length))
     }
 
-    passComplex.innerHTML = lengthPass >= 8 ? passComplexOptions[optionChekedLength] : "Weak"
+    passComplex.innerHTML = passLength >= 8 ? passComplexOptions[optionChekedLength] : "Weak"
     passGen.innerHTML = passGenerated
     optionsChecked.length = 0
 })
 
+// Copy to clipboard
 const copyToClipboard = () => {
     navigator.clipboard.writeText(passGen.innerHTML).then(() => {
         return passGen.innerHTML
